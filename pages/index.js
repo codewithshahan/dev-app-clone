e9e824fd3b4e61eb6d630074de14a8e50be6e13f
codewithshahan <programmingwithshahan@gmail.com>
2021-11-24 17:41:10 +0600

    PostFeed

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,61 @@
 import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
+import PostFeed from "../components/PostFeed";
+import { firestore, postToJSON, fromMillis } from "../lib/firebase";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
 import Loader from "../components/Loader";
-import toast from "react-hot-toast";
 
-export default function Home() {
+// Max post to query per page
+const LIMIT = 10;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <Head>
         <title>Dev Community 👩‍💻👨‍💻</title>
         <meta
@@ -17,9 +65,15 @@ export default function Home() {
         {/* <Link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <h1>Index page</h1>
+      <PostFeed posts={posts} />
 
-      <button onClick={() => toast.success("Success")}>Make a Toast</button>
-    </div>
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
   );
 }
